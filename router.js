ace274f8a5b7b4d2ce4a443f8f058aa570e723a0
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-06-09 10:30:21 +0100

    changed the basic endpoint to server/spi

--- router.js
+++ router.js
@@ -12,5 +12,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-app.use('/', require('./controllers/index'));
-app.use('/users', require('./controllers/users'));
+app.use('/api', require('./controllers/index'));
+app.use('/api/users', require('./controllers/users'));
