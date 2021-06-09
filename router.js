cabd07e729c1eeb70dc28dfb85bd48bcb9e864dd
Abdur Rehman <wabdulrehman@yahoo.com>
2021-06-09 14:07:17 +0100

    sections route

--- router.js
+++ router.js
@@ -15,3 +15,4 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.use('/', require('./controllers/index'));
 app.use('/api/users', require('./controllers/users'));
 app.use('/api/stalls', require('./controllers/stalls'));
+app.use('/api/sections', require('./controllers/sections'));
