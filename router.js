aa1543a15a4267d40426567ad4da0231e85020bc
Abdur Rehman <wabdulrehman@yahoo.com>
2021-06-09 11:45:34 +0100

    stalls route set

--- router.js
+++ router.js
@@ -14,3 +14,4 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.use('/', require('./controllers/index'));
 app.use('/api/users', require('./controllers/users'));
+app.use('/api/stalls', require('./controllers/stalls'));
